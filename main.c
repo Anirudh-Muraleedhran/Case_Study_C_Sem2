828b55bf10df4aa732bba3a57bbca5c1e01e48d7
Anirudh-Muraleedhran <163332611+Anirudh-Muraleedhran@users.noreply.github.com>
2024-04-22 19:18:15 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,56 +2,69 @@
 #include <string.h>
 #include <stdlib.h>
 
+struct Student {
+    char Name[50] ;
+    int roll;
+    int C_marks[3];
+    int Phy_marks[3];
+    int Math_marks[3];
+}; //struct Student stu[10];
 
+// array of stuctures 
+//struct Students stu[10];
+int choice;
 
 void menu_disp();
 void Adding_student();
-void display_grade();
 void get_grade();
 char grading(int x,int y,int z);
- 
-//int students=0;
-//int marks_C[10][3] = {};
-//int marks_phy[10][3] = {};
-//int marks_Math[10][3] = {};
+void disp(int i);
+void display_grade_student_wise();
+void display_all();
 
-int main() 
+int main()
 {
+
+    //struct Student stu[10];
+
+
     menu_disp();
     int input;
     printf("Enter the choice : ");
     scanf("%d",&input);
     system("cls");
 
-    //int students = 0;
     if(input == 1)
     {
         Adding_student();
-        //students += 1;
         get_grade();
+        //student_no += 1;
+        system("cls");
         main();
     }
     else if(input == 2)
     {
-        /*if(students == 0)
-        {
-            printf("No Students Added\n");
-            main();
-        }
-        */
-        {
-            system("cls");
-            display_grade();
-            main();
-        }
+        printf("1.Choose Student\n");
+        printf("2.Display All Student\n");
+        printf("Enter the choice : ");
 
+        scanf("%d",&choice);
+
+        if(choice ==1)
+        {   
+            display_grade_student_wise();
+            main();
+        }
+        else if(choice == 2)
+        {
+            display_all();
+            main();
+        }
     }
     else if(input == 3)
     {
-        system("cls");
         printf("..........EXITING............");
     }
 
-    return 0;
-    //printf("Students :%d",students);
+    //printf("%d",stu[0].C_marks[0]);
 }
